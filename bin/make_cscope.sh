4f41b0385d728d1260064ef8272d3953c91b8051
Mr.Holyshit <linuxownz@gmail.com>
2025-04-21 13:17:10 -0400

    make_cscope.sh updated to make server and client
    cscope_(client|server).out files
    
      Changes to be committed:
     	modified:   make_cscope.sh

--- bin/make_cscope.sh
+++ bin/make_cscope.sh
@@ -4,12 +4,23 @@ set -eu
 
 cd $(git rev-parse --show-toplevel)/code
 
-# remove previous db
-[ -e cscope.out ] && rm cscope.out
+# remove previous dbs
+[ -e cscope_client.out ] && rm cscope_client.out
+[ -e cscope_server.out ] && rm cscope_server.out
 
-# find client C files
-find ./botlib ./cgame ./client ./game ./qcommon ./renderercommon ./renderergl2 ./sdl ./sys  -name '*.c' | grep -Ev 'server\.c|snd_codec_ogg' > cscope.files
+# client C files
+make -f ../Makefile.client listsourcefiles | sed -e 's/code\//\n/g' > cscope_client.files
+
+# server C files
+make -f ../Makefile.server listsourcefiles | sed -e 's/code\//\n/g' > cscope_server.files
 
 # build db
-cscope -q -b -c -i cscope.files
+cscope -q -b -c -i cscope_client.files
+
+rm cscope.in.out cscope.po.out cscope_client.files
+mv cscope.out cscope_client.out
+
+cscope -q -b -c -i cscope_server.files
+rm cscope.in.out cscope.po.out cscope_server.files
+mv cscope.out cscope_server.out
 
