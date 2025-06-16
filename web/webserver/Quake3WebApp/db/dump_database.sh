09a3529f6180a5b283dda9de3086aedb565b0908
Mr.Holyshit <linuxownz@gmail.com>
2025-06-16 16:20:18 -0400

    updated
      Changes to be committed:
     	modified:   web/webserver/Quake3WebApp/db/dump_database.sh
     	modified:   web/webserver/Quake3WebApp/db/quake3webapp_server.db
    
    truncate q3serverlog before dumping
    fixed backspace bind bug
    
        IF config_value = 'BACKSPACE' THEN
            RAISE NOTICE 'deleting all binds with name = % ', config_name;
            DELETE FROM binds WHERE name = config_name >>>>> and owner = config_owner <<<<< ;
            RETURN true;
        END IF;

--- web/webserver/Quake3WebApp/db/dump_database.sh
+++ web/webserver/Quake3WebApp/db/dump_database.sh
@@ -4,6 +4,7 @@
 # TODO vacuum first
 #pg_dump -U <you> -d quake3webapp -h localhost > ./quake3webapp_server_${date}.db
 
-psql    -U <you> -d quake3webapp -h localhost -c 'vacuum;'
-pg_dump -U <you> -d quake3webapp -h localhost > ./quake3webapp_server.db
+psql    -U campbell -d quake3webapp -h localhost -c 'vacuum;'
+psql    -U campbell -d quake3webapp -h localhost -c 'truncate q3serverlog;'
+pg_dump -U campbell -d quake3webapp -h localhost > ./quake3webapp_server.db
 
